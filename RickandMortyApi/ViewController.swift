4370dcfee1e009077155073b9c5d62a84176a61e
Arthur <arthur.lee290499@gmail.com>
2021-08-06 20:38:28 +0600

    Initial Commit

---
+++ RickandMortyApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMortyApi
+//
+//  Created by Arthur Lee on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
